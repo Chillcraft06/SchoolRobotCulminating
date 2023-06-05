c24dd8a633f536cec216120236974064ac8e0382
Chillcraft <94008987+Chillcraft06@users.noreply.github.com>
2023-06-05 15:17:24 -0400

    linefollow code
    wrote entirely new code for the line following stuff

---
+++ linefollow/linefollow.ino
@@ -0,0 +1,58 @@
+int sensor = 13;
+int motor1WireA = 5;
+int motor1WireB = 6;
+
+// Probably for ground
+int motor2WireA = 10;
+int motor2WireB = 11;
+
+int Speed = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(sensor, INPUT);
+
+  pinMode(motor1WireA, OUTPUT);
+  pinMode(motor2WireA, OUTPUT);
+  pinMode(motor1WireB, OUTPUT);
+  pinMode(motor2WireB, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(sensor) == HIGH)
+  {
+      digitalWrite(motor1WireA, LOW);
+      analogWrite(motor1WireB, 255 / Speed);
+      
+      analogWrite(motor2WireA, 255 / Speed);
+      digitalWrite(motor2WireB, LOW);
+  }
+  else
+  {
+    analogWrite(motor1WireA, 255 / Speed);
+    digitalWrite(motor1WireB, LOW);
+      
+    digitalWrite(motor2WireA, LOW);
+    analogWrite(motor2WireB, 255 / Speed);
+  }
+   delay(50);
+
+    digitalWrite(motor1WireA, LOW);
+    digitalWrite(motor1WireB, LOW);
+      
+    digitalWrite(motor2WireA, LOW);
+    digitalWrite(motor2WireB, LOW);
+
+   delay(400);
+   
+   analogWrite(motor1WireA, 255 / Speed);
+   digitalWrite(motor1WireB, LOW);
+
+   analogWrite(motor2WireA, 255 / Speed);
+   digitalWrite(motor2WireB, LOW);
+
+   delay(50);
+}
