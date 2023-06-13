df7855a14f6356f760184635c335142c6cfa0775
Chillcraft <94008987+Chillcraft06@users.noreply.github.com>
2023-06-13 17:16:52 -0400

    attempted to make work by adding code
    but doesnt initialize :(

--- objectdetection/objectdetection.ino
+++ objectdetection/objectdetection.ino
@@ -1,9 +1,10 @@
-
+#include <Wire.h>
+#include <VL53L0X.h>
 
 //There are two pins for each motor whose
 //state will determing the state of movement
 //for that motor (still, running, power, direction)
-int motor1WireA = 5;
+int motor1WireA = 11;
 int motor1WireB = 6;
 
 // Probably for ground
@@ -11,6 +12,7 @@ int motor2WireA = 10;
 int motor2WireB = 11;
 
 
+VL53L0X sensor;
 
 void setup() {
   //I will setup and demonstrate one motor here.
@@ -21,8 +23,35 @@ void setup() {
   pinMode(motor2WireA, OUTPUT);
   pinMode(motor2WireB, OUTPUT);
 
+  delay(500);
 
+  Serial.begin(115200);
+
+  Wire.begin();
+
+  sensor.setTimeout(500);
   
+   if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1) {}
+  }
+
+#if defined LONG_RANGE
+  // lower the return signal rate limit (default is 0.25 MCPS)
+  sensor.setSignalRateLimit(0.1);
+  // increase laser pulse periods (defaults are 14 and 10 PCLKs)
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+#endif
+
+#if defined HIGH_SPEED
+  // reduce timing budget to 20 ms (default is about 33 ms)
+  sensor.setMeasurementTimingBudget(20000);
+#elif defined HIGH_ACCURACY
+  // increase timing budget to 200 ms
+  sensor.setMeasurementTimingBudget(200000);
+#endif
 }
 void AbsForwards(int ms, int intensity)
 {
@@ -117,6 +146,17 @@ void Turn(int Degrees, int intensity)
   digitalWrite(motor2WireB, LOW);
 }
 void loop() {
+  if ((sensor.readRangeSingleMillimeters() / 10) <= 8)
+  {
+    Turn(260, 100);
+  }
+  else
+  {
+    AbsBackwards(100, 100);
+  }
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+  delay(100);
 }
 
 
