a6e427a57d0014866074de5c8213436e7b598725
Chillcraft <94008987+Chillcraft06@users.noreply.github.com>
2023-06-14 15:23:43 -0400

    use line sensor instead lmao
    makes it work by line sensor, doesnt work often but it does!!1!!!1

--- objectdetection/objectdetection.ino
+++ objectdetection/objectdetection.ino
@@ -1,18 +1,17 @@
-#include <Wire.h>
-#include <VL53L0X.h>
+
 
 //There are two pins for each motor whose
 //state will determing the state of movement
 //for that motor (still, running, power, direction)
-int motor1WireA = 11;
+int motor1WireA = 5;
 int motor1WireB = 6;
 
 // Probably for ground
 int motor2WireA = 10;
 int motor2WireB = 11;
 
+int sensor = 4;
 
-VL53L0X sensor;
 
 void setup() {
   //I will setup and demonstrate one motor here.
@@ -23,35 +22,8 @@ void setup() {
   pinMode(motor2WireA, OUTPUT);
   pinMode(motor2WireB, OUTPUT);
 
-  delay(500);
+  pinMode(sensor, INPUT);
 
-  Serial.begin(115200);
-
-  Wire.begin();
-
-  sensor.setTimeout(500);
-  
-   if (!sensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1) {}
-  }
-
-#if defined LONG_RANGE
-  // lower the return signal rate limit (default is 0.25 MCPS)
-  sensor.setSignalRateLimit(0.1);
-  // increase laser pulse periods (defaults are 14 and 10 PCLKs)
-  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
-  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
-#endif
-
-#if defined HIGH_SPEED
-  // reduce timing budget to 20 ms (default is about 33 ms)
-  sensor.setMeasurementTimingBudget(20000);
-#elif defined HIGH_ACCURACY
-  // increase timing budget to 200 ms
-  sensor.setMeasurementTimingBudget(200000);
-#endif
 }
 void AbsForwards(int ms, int intensity)
 {
@@ -146,15 +118,15 @@ void Turn(int Degrees, int intensity)
   digitalWrite(motor2WireB, LOW);
 }
 void loop() {
-  if ((sensor.readRangeSingleMillimeters() / 10) <= 8)
+  if (digitalRead(sensor) == HIGH)
   {
-    Turn(260, 100);
+    Turn(270, 100);
   }
   else
   {
     AbsBackwards(100, 100);
   }
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
 
   delay(100);
 }
